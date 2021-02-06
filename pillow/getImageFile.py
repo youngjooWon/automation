6ee5bb1f56c0b92dc15f720d7ec690b625608120
youngjooWon <wyj312@gmail.com>
2021-02-06 22:35:45 +0900

    finally done!!!

--- pillow/getImageFile.py
+++ pillow/getImageFile.py
@@ -18,19 +18,13 @@ from moviepy.video.io.VideoFileClip import VideoFileClip as mpe
 import ffmpeg
 import natsort
 
-# from PyInstaller.utils.hooks import collect_data_files
-# datas = collect_data_files('grpc')
-
-#  from PyInstaller.utils.hooks import collect_data_files
-#  datas = collect_data_files('PyInstaller')
- #datas, binaries, hiddenimports = collect_all('PyInstaller')
 import sys
 
 if getattr(sys, 'frozen', False):
     os.environ['SSL_CERT_FILE'] = os.path.join(sys._MEIPASS, 'lib', 'cert.pem')
 
 #data_only=Ture로 해줘야 수식이 아닌 값으로 받아온다.
-load_wb = load_workbook("C:\\ttsInExcel\include\sampleFile1.xlsx", data_only=True)
+load_wb = load_workbook("C:\\ttsInExcel\include\excelFile.xlsx", data_only=True)
 #시트 이름으로 불러오기
 load_ws = load_wb['Sheet1']
 max_row = load_ws.max_row
@@ -77,7 +71,7 @@ def ssml_to_audio(ssml_text, outfile, lang):
         out.write(response.audio_content)
 
 def text_to_ssml(inputfile, speed):
-
+    
     raw_lines = inputfile
 
     escaped_lines = html.escape(raw_lines)
@@ -85,9 +79,11 @@ def text_to_ssml(inputfile, speed):
     # Convert plaintext to SSML
     # Wait two seconds between each address
     ssml = "<speak><prosody rate='"+ speed + "'>{}</prosody></speak>".format(
-        escaped_lines.replace("0 ", '<break time="2s"/>').replace("1 ", '<break time="1.5s"/>').replace("2 ", '<break time="1s"/>').replace("3 ", '<break time="0.7s"/>')
+        escaped_lines.replace("  ", '<break time="0.5s"/>').replace("# ", '<break time="0.25s"/>').replace("* ", '<break time="2.2s"/>').replace("0 ", '<break time="2s"/>').replace("1 ", '<break time="1.5s"/>').replace("2 ", '<break time="1s"/>').replace("3 ", '<break time="0.7s"/>')
     )
 
+    print(ssml)
+ 
     # Return the concatenated string of ssml script
     return ssml
 
@@ -96,44 +92,64 @@ print("파일 읽는 중 입니다..")
 for i in load_ws.rows:
     try:
         target_image = Image.open('C:\\ttsInExcel\\include\\background.jpg')  #일단 기본배경폼 이미지를 open 합니다.
-        draw = ImageDraw.Draw(target_image) 
+        draw = ImageDraw.Draw(target_image)
+        num = i[1].value
         eng = i[3].value
         kor = i[4].value
         word = eng + " : " + kor
 
-        print(str(count) + ". " + word)
+        print()
+        print(str(num) + ". " + word)
         
-        imgNm = "C:\\ttsInExcel\png" + str(count) + ".png"
-        audioNm = "C:\\ttsInExcel\\audio" + str(count) + ".mp3"
-        audioKoNm = "C:\\ttsInExcel\\audioKo" + str(count) + ".mp3"
+        imgNm = "C:\\ttsInExcel\png" + str(num) + ".png"
+        audioNm = "C:\\ttsInExcel\\audio" + str(num) + ".mp3"
+        audioKoNm = "C:\\ttsInExcel\\audioKo" + str(num) + ".mp3"
         
-        draw.text((80, 50), str(count), font=ImageFont.truetype("SCDream7.otf", 35), fill=(160,161,157,a))
+        draw.text((80, 50), str(num), font=ImageFont.truetype("SCDream7.otf", 35), fill=(160,161,157,a))
 
         if len(eng) >= 13 or len(kor) >= 7:
-            if len(eng) >= 13 :
+            if len(eng) >= 18 :
                 draw.text((130, 310), eng, font=selectedFont, fill=(b,g,r,a))
+            elif len(eng) >= 13 :
+                draw.text((250, 310), eng, font=selectedFont, fill=(b,g,r,a))        
             elif len(eng) >= 9 : 
-                draw.text((320, 310), eng, font=selectedFont, fill=(b,g,r,a))
+                draw.text((330, 310), eng, font=selectedFont, fill=(b,g,r,a))
             else:
                 draw.text((400, 310), eng, font=selectedFont, fill=(b,g,r,a))
             
             if len(kor) >= 16 :
                 draw.text((720, 310), kor[0:17], font=selectedFont, fill=(b,g,r,a))
                 draw.text((720, 410), kor[17:], font=selectedFont, fill=(b,g,r,a))
+               
                 kor = kor + "3 "
             else :
                 draw.text((720, 310), kor, font=selectedFont, fill=(b,g,r,a))
+                
                 kor = kor + "2 "
             
-            eng = eng + "1 " 
+            if len(eng) <= 4 :
+                eng = eng + "* "
+            else : 
+                eng = eng + "1 " 
             
         else :
-            draw.text((400, 310), eng, font=selectedFont, fill=(b,g,r,a))
-            draw.text((700, 310), kor, font=selectedFont, fill=(b,g,r,a))
-            eng = eng + "0 "
-            kor = kor + "0 "
+            if len(eng) >= 11 :
+                draw.text((330, 310), eng, font=selectedFont, fill=(b,g,r,a))
+                eng = eng + "2 "
+            elif len(eng) <= 9 and len(eng) >= 5:
+                draw.text((380, 310), eng, font=selectedFont, fill=(b,g,r,a))
+                eng = eng + "1 "
+            else :
+                draw.text((400, 310), eng, font=selectedFont, fill=(b,g,r,a))
+                eng = eng + "0 "
+            draw.text((700, 310), kor, font=selectedFont, fill=(b,g,r,a))   
+
+            if len(kor) < 4 :
+                kor = kor + "* "
+            else : 
+                kor = kor + "0 "
         
-        target_image.save(imgNm,"PNG") # 편집된 이미지 저장
+        target_image.save(imgNm) # 편집된 이미지 저장
         
         ssml = text_to_ssml(eng, "100%")
         if len(kor) >= 10 :
@@ -152,7 +168,7 @@ for i in load_ws.rows:
         combined = sound1 + sound2
 
         # save the result
-        combined.export("C:\\ttsInExcel\\audio_mixed_sounds" + str(count) + ".mp3", format="mp3")
+        combined.export("C:\\ttsInExcel\\audio_mixed_sounds" + str(num) + ".mp3", format="mp3")
 
         # delete file
         os.remove(audioNm)
@@ -172,7 +188,7 @@ for i in paths :
 
 paths = natsort.natsorted(store1)
 
-pathOut = 'C:\\ttsInExcel\\all_mixed_words.mp4'
+pathOut = 'C:\\ttsInExcel\\all_mixed_words.avi'
 fps = 0.2
 
 frame_array = []
@@ -181,7 +197,11 @@ for idx , path in enumerate(paths) :
     height, width, layers = img.shape
     size = (width, height)
     frame_array.append(img)
-out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+out = cv2.VideoWriter(pathOut,fourcc, fps, size)
+
 for i in range(len(frame_array)):
     # writing to a image array
     out.write(frame_array[i])
@@ -203,7 +223,7 @@ for file in natsort.natsorted(os.listdir(directory)):
         original_path = directory + file
         os.remove(original_path)
     
-combine_audio("C:\\ttsInExcel\\all_mixed_words.mp4","C:\\ttsInExcel\\all_mixed_sounds.mp3","C:\\ttsInExcel\\ttsVideo.mp4")
+combine_audio("C:\\ttsInExcel\\all_mixed_words.avi","C:\\ttsInExcel\\all_mixed_sounds.mp3","C:\\ttsInExcel\\ttsVideo.mp4")
 
 for file in natsort.natsorted(os.listdir(directory)):
     if file.startswith('all_mixed'):
